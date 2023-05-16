120b6ad809e85ae94d4ada9e83964b8552c957af
yru <grea2yru@gmail.com>
2023-05-16 18:32:51 +0900

    feat: editText 수정

--- src/editText.ts
+++ src/editText.ts
@@ -1,22 +1,60 @@
 import * as vscode from "vscode";
 
 export function addCommaAfterText(context: vscode.ExtensionContext) {
-  context.subscriptions.push(
-    vscode.commands.registerCommand("yru-extension.addCommaAfterText", () => {
-      // Get the active text editor
-      const editor = vscode.window.activeTextEditor;
+  let command = vscode.commands.registerCommand(
+    "yru-extension.addCommaAfterText",
+    () => {
+      const activeEditor = vscode.window.activeTextEditor;
 
-      if (editor) {
-        const document = editor.document;
-        const selection = editor.selection;
+      if (activeEditor) {
+        const document = activeEditor.document;
+        const selection = activeEditor.selection;
+        const text = document.getText(selection);
+        const newlineChar = getNewlineChar();
+        const replaceText = text.split(newlineChar).join("," + newlineChar);
 
-        // Get the word within the selection
-        const word = document.getText(selection);
-        const reversed = word.split("\n").join(",\n");
-        editor.edit((editBuilder) => {
-          editBuilder.replace(selection, reversed);
+        activeEditor.edit((editBuilder: vscode.TextEditorEdit) => {
+          editBuilder.replace(selection, replaceText);
         });
       }
-    })
+    }
   );
+
+  context.subscriptions.push(command);
 }
+
+export function replaceMybatisInsertValues(context: vscode.ExtensionContext) {
+  let command = vscode.commands.registerCommand(
+    "yru-extension.replaceMybatisInsertValues",
+    () => {
+      const activeEditor = vscode.window.activeTextEditor;
+
+      if (activeEditor) {
+        const document = activeEditor.document;
+        const selection = activeEditor.selection;
+        const text = document.getText(selection);
+        const replaceText = text
+          .replace(/[-_]([a-zA-Z])/g, (group) =>
+            group.toUpperCase().replace("-", "").replace("_", "")
+          )
+          .replace(/([a-zA-Z]+)/g, (group) =>
+            group.replace(group, "#{" + group + "}")
+          );
+
+        activeEditor.edit((editBuilder: vscode.TextEditorEdit) => {
+          editBuilder.replace(selection, replaceText);
+        });
+      }
+    }
+  );
+
+  context.subscriptions.push(command);
+}
+
+let getNewlineChar = () => {
+  if (process.platform === "win32") {
+    return "\r\n";
+  } else {
+    return "\n";
+  }
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { addCommaAfterText } from "./editText";
+import * as editText from "./editText";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -24,7 +24,8 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(disposable);
 
-  addCommaAfterText(context);
+  editText.addCommaAfterText(context);
+  editText.replaceMybatisInsertValues(context);
 }
 
 // This method is called when your extension is deactivated
